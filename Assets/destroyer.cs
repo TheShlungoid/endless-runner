a591b0c1be06859490b5ab4cf8d8252b1491f711
TheShlungoid <dwyman2024@elam.co.uk>
2024-09-19 11:34:05 +0100

    fixed bug

--- Assets/destroyer.cs
+++ Assets/destroyer.cs
@@ -5,14 +5,11 @@ using UnityEngine;
 public class destroyer : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (collision.gameObject.tag == "CleanUp")
+        {
+            Destroy(collision.gameObject);
+        }
     }
 }
