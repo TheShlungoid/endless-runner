79d720c4e5473805b3ff93b0a7203183b1894c6a
TheShlungoid <dwyman2024@elam.co.uk>
2024-10-09 12:46:07 +0100

    doneish

---
+++ Assets/glu.cs
@@ -0,0 +1,147 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Glu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(0);
+        Time.timeScale = 1;
+
+        //Takes you to Standard
+    }
+    public void Home()
+    {
+        SceneManager.LoadSceneAsync(1);
+        Time.timeScale = 1;
+
+        //Takes you to title
+    }
+    public void Options()
+    {
+        SceneManager.LoadSceneAsync(2);
+        Time.timeScale = 1;
+        Debug.Log(Time.timeScale);
+        //Takes you to title
+    }
+    public void FGpg1()
+    {
+        SceneManager.LoadSceneAsync(3);
+        Time.timeScale = 1;
+
+        //Takes you to title
+    }
+    public void FGpg2()
+    {
+        SceneManager.LoadSceneAsync(4);
+        Time.timeScale = 1;
+
+        //Takes you to title
+    }
+    public void FGpg3()
+    {
+        SceneManager.LoadSceneAsync(5);
+        Time.timeScale = 1;
+
+        //Takes you to title
+    }
+    public void FGpg4()
+    {
+        SceneManager.LoadSceneAsync(6);
+        Time.timeScale = 1;
+
+        //Takes you to title
+    }
+    public void FGpg5()
+    {
+        SceneManager.LoadSceneAsync(7);
+        Time.timeScale = 1;
+
+        //Takes you to title
+    }
+    public void FGpg6()
+    {
+        SceneManager.LoadSceneAsync(8);
+        Time.timeScale = 1;
+
+        //Takes you to title
+    }
+    public void FGpg7()
+    {
+        SceneManager.LoadSceneAsync(9);
+        Time.timeScale = 1;
+
+        //Takes you to title
+    }
+    public void FGpg8()
+    {
+        SceneManager.LoadSceneAsync(10);
+        Time.timeScale = 1;
+
+        //Takes you to title
+    }
+    public void Night()
+    {
+        SceneManager.LoadSceneAsync(11);
+        Time.timeScale = 1;
+
+        //Takes you to title
+    }
+    public void Credits()
+    {
+        SceneManager.LoadSceneAsync(12);
+        Time.timeScale = 1;
+
+        //Takes you to title
+    }
+    public void Sunset()
+    {
+        SceneManager.LoadSceneAsync(13);
+        Time.timeScale = 1;
+
+        //Takes you to title
+    }
+    public void Storm()
+    {
+        SceneManager.LoadSceneAsync(14);
+        Time.timeScale = 1;
+
+        //Takes you to title
+    }
+    public void Sun()
+    {
+        SceneManager.LoadSceneAsync(15);
+        Time.timeScale = 1;
+
+        //Takes you to title
+    }
+    public void TGNight()
+    {
+        SceneManager.LoadSceneAsync(16);
+        Time.timeScale = 1;
+
+        //Takes you to title
+    }
+    public void TGSunset()
+    {
+        SceneManager.LoadSceneAsync(17);
+        Time.timeScale = 1;
+
+        //Takes you to title
+    }
+    public void TGSun()
+    {
+        SceneManager.LoadSceneAsync(18);
+        Time.timeScale = 1;
+
+        //Takes you to title
+    }
+    public void TGStorm()
+    {
+        SceneManager.LoadSceneAsync(19);
+        Time.timeScale = 1;
+
+        //Takes you to title
+    }
+}
